fe7a8f174481c345a2e96f96dbfd27baf47e1022
Christian Rotzoll <ch.rotzoll@gmail.com>
2020-11-06 17:26:55 +0100

    Fix parsing of axios.post response
    The response is nested one data deeper.
    It nonetheless worked, because django sets a cookie with the response
    and then uses that to authenicate the user instead of HTTP auth.

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -5,13 +5,12 @@ const routes: RouteConfig[] = [
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/Login.vue') },
+      { path: '', component: () => import('pages/Login.vue') }
       // { path: 'dashboard', component: () => import('pages/Dashboard.vue') },
     ]
   },
 
-  // Always leave this as last one,
-  // but you can also remove it
+  // map all other requests to 404
   {
     path: '*',
     component: () => import('pages/Error404.vue')
