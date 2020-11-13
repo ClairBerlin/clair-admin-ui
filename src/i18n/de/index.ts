cd7d457a50d6aece3c8a428cfc2d98472ded8ec8
Christian Rotzoll <ch.rotzoll@gmail.com>
2020-11-13 08:44:46 +0100

    Add german translations
    * Forgot to `git add` them

---
+++ src/i18n/de/index.ts
@@ -0,0 +1,32 @@
+// This is just an example,
+// so you can safely delete all default props below
+
+export default {
+  // Login
+  Login: 'Anmeldung',
+  Username: 'Nutzername',
+  Password: 'Passwort',
+  Submit: 'einloggen',
+  'Please enter your username': 'Bitte geben Sie Ihren Nuternamen ein',
+  'Please enter your password': 'Bitte geben Sie Ihr Passwort ein',
+  'Login failed': 'Anmeldung ist fehlgeschlagen',
+
+  // Logout
+  'Logout failed': 'Abmeldung ist fehlgeschlagen',
+
+  // Menu
+  Overview: 'Überblick',
+  Organisation: 'Organisation',
+  Locations: 'Orte',
+  Rooms: 'Räume',
+  Sensors: 'Sensoren',
+  Settings: 'Einstellungen',
+  Feedback: 'Feedback',
+  Help: 'Hilfe',
+
+  // 404
+  'Oops. Nothing here...': 'Wie Sie sehen, sehen Sie nichts...',
+
+  // Dashboard
+  'Go Home': 'Nach Hause telefonieren'
+};
