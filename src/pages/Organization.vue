662ef5d83d7be234e1d832683295398868d62f53
Ulrich Schuster <ulrich.schuster@koing.de>
2021-01-20 07:53:35 +0100

    load users reactively upon organization load

--- src/pages/Organization.vue
+++ src/pages/Organization.vue
@@ -23,7 +23,9 @@
                 <q-item-section>
                   <q-icon name="perm_identity" />
                 </q-item-section>
-                <q-item-section> {{ pers.attributes.username }} </q-item-section>
+                <q-item-section>
+                  {{ pers.attributes.username }}
+                </q-item-section>
               </q-item>
             </q-list>
           </q-expansion-item>
@@ -34,7 +36,7 @@
 </template>
 
 <script>
-import { mapActions, mapGetters } from 'vuex'
+import { mapActions, mapGetters } from 'vuex';
 export default {
   name: 'Organizations',
   data() {
@@ -42,13 +44,18 @@ export default {
       usersIsLoading: false, // Loading of user data under way?
       usersIsError: null, // Did fetching user data end with an error?
       orgUsers: [] // Users of all organizations the authenticated user is a member of.
-    }
+    };
   },
-  created() {
-    // Fetch data from the API when the view is created and the data is
-    // already being observed. Option 1 in the Vue Router documentation here:
-    // https://router.vuejs.org/guide/advanced/data-fetching.html#fetching-after-navigation
-    this.fetchUsers()
+  watch: {
+    // Fetch the users of all organizations the currently authenticated users is a 
+    // member of. Because the organizations themselves are loaded asynchronously, watch 
+    // them and load the users once the organizations are available.
+    allOrganizations: {
+      immediate: true, // Trigger user fetch, n case organizations are already loaded.
+      handler: async function(newOrgs) {
+        this.fetchUsers();
+      }
+    }
   },
   computed: {
     ...mapGetters({
@@ -64,17 +71,18 @@ export default {
     }),
     async fetchUsers() {
       // For now, assume that the organizations are fetched already.
-      this.usersIsLoading = true
-      for (const org of this.allOrganizations) {
+      this.usersIsLoading = true;
+      const organizations = this.allOrganizations;
+      for (const org of organizations) {
         // This loop will execute in order, despite the asynchronous fetch.
         // See https://lavrton.com/javascript-loops-how-to-handle-async-await-6252dd3c795/
-        const parent = { type: 'organizations', id: org.id }
-        await this.loadRelatedUsers({ parent })
-        const user = this.getRelatedUsers({ parent })
-        this.orgUsers[org.id] = user
+        const parent = { type: 'organizations', id: org.id };
+        await this.loadRelatedUsers({ parent });
+        const user = this.getRelatedUsers({ parent });
+        this.orgUsers[org.id] = user;
       }
-      this.usersIsLoading = false
-    },
+      this.usersIsLoading = false;
+    }
   }
-}
+};
 </script>
